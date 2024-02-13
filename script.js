a7905d0cf5709dcb5d7c7988ced68fe80eaa971d
ElkhanKarimov <elxan0409@gmail.com>
2024-02-13 19:31:30 +0400

    complated task

---
+++ script.js
@@ -0,0 +1,56 @@
+// task 1 CreateElement
+
+// const header = document.getElementById("box")
+// const nav = document.createElement("nav")
+// const logo = document.createElement("logo")
+// logo.textContent = "Logo"
+// const a1 = document.createElement("a")
+// const a2 = document.createElement("a")
+// const a3 = document.createElement("a")
+// const a4 = document.createElement("a")
+// const a5 = document.createElement("a")
+// const menuLinks = document.createElement("menuLinks")
+
+// header.appendChild(nav)
+// nav.appendChild(logo)
+// menuLinks.appendChild(a1)
+// menuLinks.appendChild(a2)
+// menuLinks.appendChild(a3)
+// menuLinks.appendChild(a4)
+// menuLinks.appendChild(a5)
+// nav.appendChild(menuLinks)
+
+// a1.textContent = "Home"
+// a2.textContent = "About"
+// a3.textContent = "Contact"
+// a4.textContent = "Tel"
+// a5.textContent = "Social"
+// a1.style.color = "white"
+// a2.style.color = "white"
+// a3.style.color = "white"
+// a4.style.color = "white"
+// a5.style.color = "white"
+// a1.href = "Home"
+// a2.href = "About"
+// a3.href = "Contact"
+// a4.href = "Tel"
+// a5.href = "Social"
+// a1.style.textDecoration = "none"
+// a2.style.textDecoration = "none"
+// a3.style.textDecoration = "none"
+// a4.style.textDecoration = "none"
+// a5.style.textDecoration = "none"
+// nav.style.display = "flex"
+// nav.style.justifyContent = "space-between"
+// nav.style.fontSize = "20px"
+// nav.style.padding = "50px"
+// nav.style.backgroundColor = "blue"
+// nav.style.color = "white"
+// menuLinks.style.display = "flex"
+// menuLinks.style.width = "300px"
+// menuLinks.style.justifyContent = "space-around"
+
+
+
+
+
---
+++ script2.js
@@ -0,0 +1,16 @@
+// task 1 InnerHTML
+
+
+// const box = document.getElementById("box")
+// box.innerHTML = ` <nav style="display: flex; justify-content: space-between;font-size: 30px; padding: 50px; background-color: brown; color: white;">
+// <div class="logo" style="display: flex; align-items: center; justify-content: center;">Logo</div>
+// <div class="menuLinks" style="text-decoration:none ; display: flex;">
+//     <ul style="list-style: none; display: flex; width: 500px; justify-content: space-around;">
+//         <li><a style="text-decoration:none ; color: white;" href="">Home</a></li>
+//         <li><a style="text-decoration:none ; color: white;" href="">About</a></li>
+//         <li><a style="text-decoration:none ; color: white;" href="">Contact</a></li>
+//         <li><a style="text-decoration:none ; color: white;" href="">Tel</a></li>
+//         <li><a style="text-decoration:none ; color: white;" href="">Social</a></li>
+//     </ul>
+// </div>
+// </nav>`
